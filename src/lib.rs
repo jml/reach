c02b5f9167d0a3eb9088c3c20c37d5409ac31a65
Jonathan Lange <jml@mumak.net>
2022-03-19 17:52:51 +0000

    Move the stdin logic into the trait implementation

--- src/lib.rs
+++ src/lib.rs
@@ -69,52 +69,37 @@ impl StdinRunner {
 #[async_trait]
 impl Runner for StdinRunner {
     async fn run(&self, source_file: &fs::DirEntry) -> io::Result<()> {
-        run_process_stdin(
-            source_file,
-            &self.destination_dir,
-            &self.shell,
-            &self.command,
-        )
-        .await
+        // TODO(jml): This function has potential for internal parallelism.
+        // Better understand how join! and .await work and see if there's any benefit.
+
+        // TODO(jml): Understand whether this actually has any benefit over directly opening the standard file.
+        let source_path = source_file.path();
+        let in_file = fs::File::open(source_path).await?.into_std().await;
+
+        let mut base_directory = self.destination_dir.clone();
+        base_directory.push(source_file.file_name());
+
+        ensure_directory(&base_directory).await?;
+
+        // TODO(jml): 'create' truncates. Actual desired behaviour depends on 'recreate' setting.
+        let mut out_path = base_directory.clone();
+        out_path.push("out");
+        let out_file = fs::File::create(out_path).await?.into_std().await;
+
+        let mut err_path = base_directory.clone();
+        err_path.push("err");
+        let err_file = fs::File::create(err_path).await?.into_std().await;
+
+        let mut child_process = Command::new(&self.shell)
+            .arg("-c")
+            .arg(&self.command)
+            .stdin(in_file)
+            .stdout(out_file)
+            .stderr(err_file)
+            .spawn()?;
+        child_process.wait().await?;
+        Ok(())
     }
-}
-
-async fn run_process_stdin(
-    source_file: &fs::DirEntry,
-    destination_dir: &Path,
-    shell: &str,
-    command: &str,
-) -> io::Result<()> {
-    // TODO(jml): This function has potential for internal parallelism.
-    // Better understand how join! and .await work and see if there's any benefit.
-
-    // TODO(jml): Understand whether this actually has any benefit over directly opening the standard file.
-    let source_path = source_file.path();
-    let in_file = fs::File::open(source_path).await?.into_std().await;
-
-    let mut base_directory = destination_dir.to_path_buf();
-    base_directory.push(source_file.file_name());
-
-    ensure_directory(&base_directory).await?;
-
-    // TODO(jml): 'create' truncates. Actual desired behaviour depends on 'recreate' setting.
-    let mut out_path = base_directory.clone();
-    out_path.push("out");
-    let out_file = fs::File::create(out_path).await?.into_std().await;
-
-    let mut err_path = base_directory.clone();
-    err_path.push("err");
-    let err_file = fs::File::create(err_path).await?.into_std().await;
-
-    let mut child_process = Command::new(shell)
-        .arg("-c")
-        .arg(command)
-        .stdin(in_file)
-        .stdout(out_file)
-        .stderr(err_file)
-        .spawn()?;
-    child_process.wait().await?;
-    Ok(())
 }
 
 /// How the command given to `reach` gets at its input.
