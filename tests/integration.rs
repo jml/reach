9848dbad4c6923b5fe4650f893b6f29e8a93d775
Jonathan Lange <jml@mumak.net>
2022-03-20 14:10:18 +0000

    Add a test for filename runner

--- tests/integration.rs
+++ tests/integration.rs
@@ -71,3 +71,58 @@ async fn test_stdin() -> io::Result<()> {
     assert!(!destination_path.join("file2/status").exists());
     Ok(())
 }
+
+/// Basic test for filename processing happy path.
+///
+/// We use `echo {}` as our command.
+/// The destination directory has a file in `out` matching each file in our source directory.
+/// All of the `err` files are empty,
+/// and the `status` files don't exist, because we haven't implemented them.
+#[tokio::test]
+async fn test_filename() -> io::Result<()> {
+    let source = tempfile::tempdir()?;
+    let source_path = source.path();
+    let file1_path = source_path.join("file1.txt");
+    let mut file1 = fs::File::create(file1_path)?;
+    writeln!(file1, "Arbitrary content for file one")?;
+    let file2_path = source_path.join("file2.txt");
+    let mut file2 = fs::File::create(file2_path)?;
+    writeln!(file2, "Arbitrary content for file two")?;
+
+    let destination = tempfile::tempdir()?;
+    let destination_path = destination.path();
+    let each = reach::Each::new(source_path.to_path_buf(), 1, true, 0);
+    let shell = env::var("SHELL").unwrap();
+    let runner = reach::FilenameRunner::new(
+        shell,
+        "echo -n {}".to_string(),
+        destination_path.to_path_buf(),
+    );
+    each.run(&runner).await?;
+
+    let mut filenames = fs::read_dir(destination_path)?
+        .map(|res| res.map(|e| e.file_name()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+    filenames.sort();
+
+    assert_eq!(vec!["file1.txt", "file2.txt"], filenames);
+    assert_eq!(
+        source_path.join("file1.txt").to_string_lossy(),
+        String::from_utf8_lossy(&fs::read(destination_path.join("file1.txt/out"))?)
+    );
+    assert_eq!(
+        "",
+        String::from_utf8_lossy(&fs::read(destination_path.join("file1.txt/err"))?)
+    );
+    assert!(!destination_path.join("file1/status").exists());
+    assert_eq!(
+        source_path.join("file2.txt").to_string_lossy(),
+        String::from_utf8_lossy(&fs::read(destination_path.join("file2.txt/out"))?)
+    );
+    assert_eq!(
+        "",
+        String::from_utf8_lossy(&fs::read(destination_path.join("file2.txt/err"))?)
+    );
+    assert!(!destination_path.join("file2/status").exists());
+    Ok(())
+}
