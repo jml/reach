7fbeb476dce4d1ff50ac3cc33eb7e22176865215
Jonathan Lange <jml@mumak.net>
2022-04-10 15:26:32 +0100

    Report on success or failure using an emoji prefix

--- src/progress.rs
+++ src/progress.rs
@@ -1,3 +1,4 @@
+use console::Emoji;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::io;
 use std::process::ExitStatus;
@@ -11,17 +12,20 @@ pub trait Progress {
     fn task_completed(&self, result: io::Result<ExitStatus>);
 }
 
+static OK: Emoji<'_, '_> = Emoji("✅", "OK");
+static ERROR: Emoji<'_, '_> = Emoji("❌", "ERROR");
+
 impl Progress for ProgressBar {
     fn set_num_tasks(&self, tasks: usize) {
         self.set_length(tasks as u64);
     }
 
     fn task_completed(&self, result: io::Result<ExitStatus>) {
-        // TODO(jml): Actually use `result` to communicate whether the run succeeded.
         match result {
             Ok(_) => self.inc(1),
             Err(e) => {
                 self.println(format!("Error: {:?}", e));
+                self.set_prefix(format!("{} ", ERROR));
                 self.inc(1);
             }
         }
@@ -35,8 +39,10 @@ impl Progress for () {
 
 /// Construct a real progress bar for rendering to users.
 pub fn default_progress_bar() -> impl Progress {
-    ProgressBar::new(0).with_style(
-        ProgressStyle::default_bar()
-            .template("{prefix}{wide_bar} {pos}/{len} [{elapsed}<{eta}, {per_sec}]"),
-    )
+    ProgressBar::new(0)
+        .with_style(
+            ProgressStyle::default_bar()
+                .template("{prefix}{wide_bar} {pos}/{len} [{elapsed}<{eta}, {per_sec}]"),
+        )
+        .with_prefix(format!("{} ", OK))
 }
